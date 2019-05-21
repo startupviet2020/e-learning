e76be87ecf694f83e737dac519ffb91b3182c64c
luong thanh long <luongathanhlong76@gmail.com>
2019-05-21 13:16:27 +0700

    change database name

--- conf/db.conf.php
+++ conf/db.conf.php
@@ -1,7 +1,7 @@
 <?
 $_dbsettings=array(
 	"default"	=> array(
-							"master"	=> array("server" => "103.78.88.110", "user" => "root", "password" => "!Q3e4r5t", "dbname" => "einvoice"),
+							"master"	=> array("server" => "103.78.88.110", "user" => "root", "password" => "!Q3e4r5t", "dbname" => "elearning"),
 							"slaves"	=> array(),
 							"mode"	=> "DEBUG"
 						),
